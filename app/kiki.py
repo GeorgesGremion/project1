da755cf5a7a1b869262d774f437a9d04240fdb56
Georges Gremion <84907203+GeorgesGremion@users.noreply.github.com>
2023-09-04 22:43:38 +0200

    kiki test

--- app/kiki.py
+++ app/kiki.py
@@ -1,7 +1,7 @@
 from app import app, db
-from app.models import User, Child
+from app.models import User, Child, Activity
 
 
 @app.shell_context_processor
 def make_shell_context():
-        return {'db': db, 'User': User, 'Child': Child}
+        return {'db': db, 'User': User, 'Child': Child, 'Activity': Activity}
