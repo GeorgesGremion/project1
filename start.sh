3206eabe9ead054da1bcec8aa1deba1ef1631d93
Georges Gremion <84907203+GeorgesGremion@users.noreply.github.com>
2023-09-05 03:45:24 +0200

    anpassungen ablauf

--- start.sh
+++ start.sh
@@ -2,21 +2,16 @@
 
 if [ ! -f /app/.initialized ]; then
     python3 -m venv venv
+    /app/venv/bin/pip3 install -r requirements.txt
+    export FLASK_APP=/app/kiki.py
     /app/venv/bin/flask db init
     /app/venv/bin/flask db migrate
     /app/venv/bin/flask db upgrade
-    /app/venv/bin/pip3 install -r requirements.txt
-    FLASK_APP /app/kiki.py
-    service supervisor start
-    supervisorctl reload
-    supervisorctl start kiki
-    nginx -g "daemon off;"
     touch /app/.initialized
-else 
-    FLASK_APP /app/kiki.py
-    service supervisor start
-    supervisorctl reload
-    supervisorctl start kiki
-    nginx -g "daemon off;"
 fi
 
+export FLASK_APP=/app/kiki.py
+service supervisor start
+supervisorctl reload
+supervisorctl start kiki
+nginx -g "daemon off;"
