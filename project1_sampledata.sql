5e2909fcccb4615db99e1a75544198774769e3d1
Georges Gremion <84907203+GeorgesGremion@users.noreply.github.com>
2023-09-05 12:02:58 +0200

    sql hinzugefügt

---
+++ project1_sampledata.sql
@@ -0,0 +1,175 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: mysql:3306
+-- Erstellungszeit: 05. Sep 2023 um 10:01
+-- Server-Version: 8.0.34
+-- PHP-Version: 8.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `project1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `activity`
+--
+
+CREATE TABLE `activity` (
+  `id` int NOT NULL,
+  `child_id` int NOT NULL,
+  `food` varchar(50) DEFAULT NULL,
+  `sleep_start` datetime DEFAULT NULL,
+  `sleep_end` datetime DEFAULT NULL,
+  `diaper_change_large` datetime DEFAULT NULL,
+  `diaper_change_small` datetime DEFAULT NULL,
+  `timestamp` datetime DEFAULT NULL,
+  `activity_type` varchar(50) DEFAULT NULL,
+  `description` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `alembic_version`
+--
+
+CREATE TABLE `alembic_version` (
+  `version_num` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Daten für Tabelle `alembic_version`
+--
+
+INSERT INTO `alembic_version` (`version_num`) VALUES
+('b4acbd436aa1');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `child`
+--
+
+CREATE TABLE `child` (
+  `id` int NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `age` int NOT NULL,
+  `group` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Daten für Tabelle `child`
+--
+
+INSERT INTO `child` (`id`, `first_name`, `last_name`, `age`, `group`) VALUES
+(1, 'Chuck', 'Norris', 1, 'A'),
+(2, 'Arnold', 'Schwarzenegger', 2, 'A'),
+(3, 'Mel', 'Gibson', 3, 'B'),
+(4, 'Jean-Claude', 'Van Damme', 4, 'B'),
+(5, 'Nicolas', 'Cage', 5, 'C'),
+(6, 'Bruce', 'Willis', 6, 'C');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `user`
+--
+
+CREATE TABLE `user` (
+  `id` int NOT NULL,
+  `username` varchar(64) DEFAULT NULL,
+  `email` varchar(120) DEFAULT NULL,
+  `password_hash` varchar(128) DEFAULT NULL,
+  `group` varchar(50) DEFAULT NULL,
+  `last_seen` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Daten für Tabelle `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password_hash`, `group`, `last_seen`) VALUES
+(1, 'admin', 'admin@kita-app.ch', 'pbkdf2:sha256:600000$kLXKgCNW5pTZfSux$a91f3689757cb6126b90b2f27fdb228cc8586c43f644064f89d4e9787623d405', 'A', '2023-09-05 09:57:28');
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `activity`
+--
+ALTER TABLE `activity`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `child_id` (`child_id`),
+  ADD KEY `ix_activity_timestamp` (`timestamp`);
+
+--
+-- Indizes für die Tabelle `alembic_version`
+--
+ALTER TABLE `alembic_version`
+  ADD PRIMARY KEY (`version_num`);
+
+--
+-- Indizes für die Tabelle `child`
+--
+ALTER TABLE `child`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `ix_user_email` (`email`),
+  ADD UNIQUE KEY `ix_user_username` (`username`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `activity`
+--
+ALTER TABLE `activity`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `child`
+--
+ALTER TABLE `child`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT für Tabelle `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `activity`
+--
+ALTER TABLE `activity`
+  ADD CONSTRAINT `activity_ibfk_1` FOREIGN KEY (`child_id`) REFERENCES `child` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
