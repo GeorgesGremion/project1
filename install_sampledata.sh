c6721c904fa9ce311131db3c651186efed6b3a0c
Georges Gremion <84907203+GeorgesGremion@users.noreply.github.com>
2023-09-05 12:38:41 +0200

    asd

--- install_sampledata.sh
+++ install_sampledata.sh
@@ -1,2 +1,2 @@
-docker cp project1_sampledata.sql project1-mysql-1:/tmp/sample-data.sql
-docker exec -it project1-mysql-1 mysql -u project1 -pSuperSicher project1 < /tmp/sample-data.sql
+#!/bin/bash
+docker exec -i project1-mysql-1 sh -c 'mysql -u project1 -pSuperSicher project1' < ./project1_sampledata.sql
