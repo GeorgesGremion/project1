4ca0b0367afeb284e0d734d00367b2dc4a770818
Georges Gremion <84907203+GeorgesGremion@users.noreply.github.com>
2023-09-05 14:26:38 +0200

    Finale cleaning

--- app/api.py
+++ app/api.py
@@ -1,21 +1,20 @@
-from app import app
-from app.models import User, Post
 from flask import jsonify
+from flask_login import login_required, current_user
+from app.models import User, Child
 
-@app.route('/api/users/<int:id>', methods=['GET'])
-def get_user(id):
- data = User.query.get_or_404(id).to_dict()
- return jsonify(data)
-@app.route('/api/users', methods=['GET'])
-def get_users():
- data = User.to_collection()
- return jsonify(data)
-@app.route('/api/users/<int:id>/followers', methods=['GET'])
-def get_followers(id):
- pass
-@app.route('/api/users/<int:id>/followed', methods=['GET'])
-def get_followed(id):
- pass
-@app.route('/api/users/<int:id>/posts', methods=['GET'])
-def get_posts(id):
- pass
+def get_user_children():
+    user_group = current_user.group
+    children_in_group = Child.query.filter_by(group=user_group).all()
+
+    children_data = [
+        {
+            'id': child.id,
+            'first_name': child.first_name,
+            'last_name': child.last_name,
+            'age': child.age,
+            'group': child.group
+        }
+        for child in children_in_group
+    ]
+
+    return jsonify(children_data)
